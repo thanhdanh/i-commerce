3d69e7640ea94ceac78e99c95680c62031245039
danhvt0109 <danhvt0109@flomail.net>
2021-01-29 15:12:58 +0700

    Add health check

---
+++ gateway/src/health-check/health-check.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { HealthCheck, HealthCheckService, MicroserviceHealthIndicator } from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+    constructor(
+        private health: HealthCheckService,
+        private microservice: MicroserviceHealthIndicator
+    ) {}
+    
+    @Get()
+    @HealthCheck()
+    check() {
+        return this.health.check([
+            async () =>
+            this.microservice.pingCheck('tcp', {
+                transport: Transport.TCP,
+                options: { host: 'localhost', port: 8889 },
+            }),
+        ]) 
+    }
+}
---
+++ gateway/src/health-check/health-check.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common'
+import { HealthController } from './health-check.controller';
+@Module({
+    controllers: [HealthController]
+})
+export class HealthCheckModule { }
